5e6dbc4d69e5970e4ac2dc7512c7f46131bc1671
Nasrallah <simon_nasrallah@hotmail.com>
2013-03-22 17:11:14 +0000

    initial commit

---
+++ src/mailer.py
@@ -0,0 +1,18 @@
+__author__ = 'snasrallah'
+import smtplib
+
+fromaddr = ''
+toaddrs  = ''
+msg = 'There was a terrible error that occured and I wanted you to know!'
+
+
+# Credentials (if needed)
+username = ''
+password = ''
+
+# The actual mail send
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
