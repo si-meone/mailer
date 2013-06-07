38c08c4803ed849da98affa2295f940a9739e7b5
snasrallah <simon_nasrallah@hotmail.com>
2013-06-07 16:38:07 +0200

    Update mailer.py

--- src/mailer.py
+++ src/mailer.py
@@ -1,18 +1,83 @@
 __author__ = 'snasrallah'
 import smtplib
 
-fromaddr = ''
-toaddrs  = ''
-msg = 'There was a terrible error that occured and I wanted you to know about'
+def mailer():
+    fromaddr = ''
+    toaddrs  = ''
+    msg = 'There was a terrible error that occured and I wanted you to know about'
+    
+    
+    # Credentials (if needed)
+    username = ''
+    password = ''
+    
+    # The actual mail send
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+
+def get_ip_address():
+
+    import socket
+    # 1: Use the gethostname method
+
+    ipaddr = socket.gethostbyname(socket.gethostname())
+    if not( ipaddr.startswith('127') ) :
+        print('Can use Method 1: ' + ipaddr)
+        #return ipaddr
+
+    # 2: Use outside connection
+    '''
+    Source:
+    http://commandline.org.uk/python/how-to-find-out-ip-address-in-python/
+    '''
+
+    ipaddr=''
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('google.com', 0))
+        ipaddr=s.getsockname()[0]
+        print('Can used Method 2: ' + ipaddr)
+        return ipaddr
+    except:
+        pass
 
 
-# Credentials (if needed)
-username = ''
-password = ''
+    # 3: Use OS specific command
+    import subprocess , platform
+    ipaddr=''
+    os_str=platform.system().upper()
 
-# The actual mail send
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+    if os_str=='LINUX' :
+
+        # Linux:
+        arg='ip route list'
+        p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
+        data = p.communicate()
+        sdata = data[0].split()
+        ipaddr = sdata[ sdata.index('src')+1 ]
+        #netdev = sdata[ sdata.index('dev')+1 ]
+        print('Can used Method 3: ' + ipaddr)
+        #return ipaddr
+
+    elif os_str=='WINDOWS' :
+
+        # Windows:
+        arg='route print 0.0.0.0'
+        p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
+        data = p.communicate()
+        strdata=data[0].decode()
+        sdata = strdata.split()
+
+        while len(sdata)>0:
+            if sdata.pop(0)=='Netmask' :
+                if sdata[0]=='Gateway' and sdata[1]=='Interface' :
+                    ipaddr=sdata[6]
+                    break
+        print('Can used Method 4: ' + ipaddr)
+        #return ipaddr
+
+if __name__ == '__main__':
+    mailer()
